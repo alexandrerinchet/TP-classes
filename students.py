2a8b6a6a0d07b08ce0e28f27ef281c17555cb7d3
alexandrerinchet <alex1123@outlook.fr>
2025-12-07 21:58:46 +0100

    Add files via upload

--- students.py
+++ students.py
@@ -2,32 +2,50 @@ from collections import defaultdict
 
 class Student:
     def __init__(self, first_name: str, last_name: str, grades: dict = None) :
-        if grades is None :
-            self.grades={}
-        else :
-            self.grades=grades
-        self.first_name=first_name
-        self.last_name=last_name
+        self.first_name = first_name
+        self.last_name = last_name
+        self.grades = {}
 
     def __repr__(self):
         return f"{self.first_name} {self.last_name}"
     
     def add_grade(self, topic : str, grade : float) :
-        if not (0<=grade<=20) :
-            raise ValueError("Grade must be between 0 and 20.")
-        self.grades[topic]=grade
+        if topic not in self.grades:
+            self.grades[topic] = []
+        self.grades[topic].append(grade)
 
     def followed_topics(self) :
         return list(self.grades.keys())
     
-try:
-    student = Student("Achille", "Talon")
-    student.add_grade("History", 10.)
-    topics = student.followed_topics()
-    if len(topics) != 1 or "History" not in topics:
-        raise Exception(f"Expecting ['History'] got {topics}")
-except Exception as e:
-    print('OOPS - There is an issue in your followed_topics method')
-    print(f"Error message : {e}")
-else:
-    print('Congrats ! Your implementation works !')
+    def compute_average(self, topic: str):
+        if topic not in self.grades:
+            return -1.0
+        
+        notes_list = self.grades[topic]
+
+        total = sum(notes_list)
+        number_of_grades = len(notes_list)
+        
+        if number_of_grades == 0:
+            return 0.0
+            
+        return total / number_of_grades
+    
+    def report(self):
+        lines = []
+        lines.append(f"Report for student {self.first_name} {self.last_name}")
+        lines.append("+===============+===============+")
+        lines.append("|     Topic     |    Average    |")
+        lines.append("+===============+===============+")
+        
+        for topic in self.grades:
+            average_val = self.compute_average(topic)
+            
+            line = f"|{topic:^15}|{average_val:^15.2f}|" #pour centrer le texte
+            lines.append(line)
+            lines.append("+---------------+---------------+")
+            
+        return "\n".join(lines)
+    
+
+
